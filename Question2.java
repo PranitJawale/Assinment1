8372bbfcd3f3b66b053723f607b594f24633760c
PranitJawale <139535877+PranitJawale@users.noreply.github.com>
2025-05-12 23:44:20 +0530

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,12 @@
+public class Question2
+{
+public static void main(String [] args)
+{
+String name="Pranit";
+int age=25;
+System.out.println(" Name :"+name);
+System.out.println(" Age :"+age);
+
+}
+
+}
