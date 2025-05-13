5b143ead94c0171fa4a8d6956b4a4755e41e86b7
PranitJawale <139535877+PranitJawale@users.noreply.github.com>
2025-05-13 07:28:06 +0530

    Add files via upload
    Write a program to find and print the largest of three numbers.

---
+++ LargestOfThree.java
@@ -0,0 +1,29 @@
+
+
+public class LargestOfThree {
+    public static void main(String[] args) {
+      
+        int num1 = 26;
+
+       
+        int num2 = 36;
+
+       
+        int num3 =45;
+
+       
+        int largest;
+        if (num1 >= num2 && num1 >= num3) {
+            largest = num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            largest = num2;
+        } else {
+            largest = num3;
+        }
+
+        
+        System.out.println("The largest number is: " + largest);
+
+       
+    }
+}
